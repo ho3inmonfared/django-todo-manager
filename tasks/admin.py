be47cf4c3197bdf6d3745a015880550383fc3432
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2025-11-08 12:25:55 +0330

    add Task model, migrate DB and register admin (day 2)

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'completed', 'created_at', 'updated_at', 'user')
+    list_filter = ('completed', 'created_at', 'user')
+    search_fields = ('title', 'description','user__username')
