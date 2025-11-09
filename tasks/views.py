dd00e2e7f2f582b2f2878761f726b24ffe78aaba
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2025-11-09 12:10:05 +0330

    add TaskForm and create task functionality (day 3)

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Task
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    return render(request,'index.html',context={'title': 'To_Do Manager'})
+    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('index')
+    else:
+        form = TaskForm()
+    
+    context = {'tasks': tasks, 'form': form}
+    return render(request,'index.html',context)
