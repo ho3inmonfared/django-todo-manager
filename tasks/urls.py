80ef06f226d24b81725350c2dce9c42caa89ca5a
Hossein_Monfared <140837105+ho3inmonfared@users.noreply.github.com>
2025-11-13 08:21:19 +0330

    Add task detail view and improve feedback messages(day 6)
    Introduces a new detail view and template for individual tasks, updates URLs and index template to support task detail navigation, and adds user feedback messages for task creation, deletion, and completion actions.

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('complete/<int:task_id>/',views.complete_task,name='complete_task')
+    path('complete/<int:task_id>/',views.complete_task,name='complete_task'),
+    path('task/<int:task_id>/', views.detail_task, name='detail_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from .models import Task
 from .forms import TaskForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 @login_required
 def index(request):
@@ -14,9 +15,13 @@ def index(request):
             task = form.save(commit=False)
             task.user = request.user
             task.save()
+            messages.success(request, "Task added successfully!✔")
             return redirect('index')
+        else:
+            messages.error(request, "There was an error adding the task. Please try again.❌")
     else:
         form = TaskForm()
+        
     
     context = {'tasks': tasks, 'form': form}
     return render(request,'index.html',context)
@@ -27,6 +32,7 @@ def index(request):
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id, user=request.user)
     task.delete()
+    messages.success(request, "Task deleted successfully!✔")
     return redirect('index')
 
 @login_required
@@ -35,4 +41,11 @@ def complete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id, user=request.user)
     task.completed = not task.completed
     task.save()
+    messages.success(request, "Task status updated successfully!✔")
     return redirect('index')
+
+@login_required
+def detail_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+    context = {'task': task}
+    return render(request, 'detail.html', context)
